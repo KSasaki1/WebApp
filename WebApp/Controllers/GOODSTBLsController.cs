ac1a24f7e5fa8c7d8f813b51bb7740b852f0601f
KSasaki1 <53255392+KSasaki1@users.noreply.github.com>
2019-08-27 18:07:23 +0900

    プロジェクト ファイルを追加します。

---
+++ WebApp/Controllers/GOODSTBLsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Models
+{
+    public class GOODSTBLsController : Controller
+    {
+        private trdbEntities db = new trdbEntities();
+
+        // GET: GOODSTBLs
+        public ActionResult Index()
+        {
+            return View(db.GOODSTBL.ToList());
+        }
+
+        // GET: GOODSTBLs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GOODSTBL gOODSTBL = db.GOODSTBL.Find(id);
+            if (gOODSTBL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gOODSTBL);
+        }
+
+        // GET: GOODSTBLs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GOODSTBLs/Create
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GOODSID,GOOSNM,PRICE")] GOODSTBL gOODSTBL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GOODSTBL.Add(gOODSTBL);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(gOODSTBL);
+        }
+
+        // GET: GOODSTBLs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GOODSTBL gOODSTBL = db.GOODSTBL.Find(id);
+            if (gOODSTBL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gOODSTBL);
+        }
+
+        // POST: GOODSTBLs/Edit/5
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GOODSID,GOOSNM,PRICE")] GOODSTBL gOODSTBL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gOODSTBL).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(gOODSTBL);
+        }
+
+        // GET: GOODSTBLs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GOODSTBL gOODSTBL = db.GOODSTBL.Find(id);
+            if (gOODSTBL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gOODSTBL);
+        }
+
+        // POST: GOODSTBLs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            GOODSTBL gOODSTBL = db.GOODSTBL.Find(id);
+            db.GOODSTBL.Remove(gOODSTBL);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApp/Controllers/STOCKTBLsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp
+{
+    public class STOCKTBLsController : Controller
+    {
+        private trdbEntities db = new trdbEntities();
+
+        // GET: STOCKTBLs
+        public ActionResult Index()
+        {
+            return View(db.STOCKTBL.ToList());
+        }
+
+        // GET: STOCKTBLs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            STOCKTBL sTOCKTBL = db.STOCKTBL.Find(id);
+            if (sTOCKTBL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sTOCKTBL);
+        }
+
+        // GET: STOCKTBLs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: STOCKTBLs/Create
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DEALID,GOODSID,STOCK,LASTUDT")] STOCKTBL sTOCKTBL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.STOCKTBL.Add(sTOCKTBL);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sTOCKTBL);
+        }
+
+        // GET: STOCKTBLs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            STOCKTBL sTOCKTBL = db.STOCKTBL.Find(id);
+            if (sTOCKTBL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sTOCKTBL);
+        }
+
+        // POST: STOCKTBLs/Edit/5
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DEALID,GOODSID,STOCK,LASTUDT")] STOCKTBL sTOCKTBL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sTOCKTBL).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sTOCKTBL);
+        }
+
+        // GET: STOCKTBLs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            STOCKTBL sTOCKTBL = db.STOCKTBL.Find(id);
+            if (sTOCKTBL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sTOCKTBL);
+        }
+
+        // POST: STOCKTBLs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            STOCKTBL sTOCKTBL = db.STOCKTBL.Find(id);
+            db.STOCKTBL.Remove(sTOCKTBL);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
